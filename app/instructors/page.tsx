6cd804fdfddb5b0b1596bda4bccf54313aadcf4d
enistien <uanonymous.m47@gmail.com>
2025-08-05 09:50:12 +0300

    first commit

--- app/instructors/page.tsx
+++ app/instructors/page.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import InstructorsPage from './_components/instructor-page'
 import { prisma } from '@/lib/prisma'
-const page = async () => {
- 
-   const instructors : any = await prisma.instructor.findMany({
-        include: {
-          _count: {
-            select: { courses: true },
-          },
-        },
-        orderBy: { name: "asc" },
-      })
-      
-console.log("Fetched instructors:", instructors)
+
+export default async function Page() {
+  const instructors: any = await prisma.instructor.findMany({
+    include: {
+      _count: {
+        select: { courses: true },
+      },
+    },
+    orderBy: { name: "asc" },
+  })
+
   return (
     <div>
       <InstructorsPage instructors={instructors} />
     </div>
   )
 }
-
-export default page
